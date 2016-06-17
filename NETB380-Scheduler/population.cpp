59ef33f071389912ad38967e273bc8ef63c94f51
Fenigor <starblocer@abv.bg>
2016-06-17 16:58:25 +0300

    mutaion func

--- NETB380-Scheduler/population.cpp
+++ NETB380-Scheduler/population.cpp
@@ -1,12 +1,65 @@
 #include "population.h"
 
-Population::Population()
-{
+int POPULATION_SIZE = 10;
+int MAXGENS = 100;
+int NVARS = 6;// FOR SIX DAYS
+int MUTATION = 2;// mutation chance
 
+Population::Population(Schedule init_schedule)
+{
+    srand(unsigned(time(0)));
+    for(int i = 0;i< POPULATION_SIZE;i++)
+    {
+        this->population[i] = init_schedule;
+    }
 }
 
 Population::~Population()
+{}
+
+
+void Population::elitist()
 {
 
 }
+
+void Population::mutate()
+{
+    int lowest_fitness;
+    int current_fitness;
+    int leakest;// get index of leakest
+    int x;
+
+    lowest_fitness = population[0].get_fitness();
+
+    for(int mut = 0; mut <= POPULATION_SIZE * 20 /100; mut++)// perfom only on 20% of the population
+    {
+
+        for(int i = 0;i < POPULATION_SIZE; i++)
+        {
+            current_fitness = population[i].get_fitness();
+            if(current_fitness < lowest_fitness)
+            {
+                lowest_fitness = current_fitness;
+                leakest = i;
+            }
+        }
+        x = rng_i(10);
+        if(x < MUTATION)
+        {
+            population[leakest].randomize_schedule();
+        }
+    }
+
+}
+
+int Population::rng_i(int i)
+{
+    return rand() % i;
+}
+int Population::rng_ab(int a, int b)
+{
+    return rand() % a + b;
+}
+
 
--- NETB380-Scheduler/population.h
+++ NETB380-Scheduler/population.h
@@ -1,7 +1,17 @@
 #ifndef POPULATION_H
 #define POPULATION_H
 
+#include <vector>
+#include <ctime>
+#include <stdlib.h>
 #include "schedule.h"
+
+extern int POPULATION_SIZE;
+extern int MAXGENS;
+extern int NVARS; //6 for six days
+extern int MUTATION;// chance for mutation
+
+using namespace std;
 
 class Population
 {
@@ -9,9 +19,17 @@ class Population
 private:
     vector <Schedule> population;
     vector <Schedule> newPopulation;
+
+    static int rng_i(int i);
+    static int rng_ab(int a, int b);
 public:
-    Population();
+    Population(Schedule init_schedule);
     ~Population();
+
+    void elitist();
+
+    void mutate();
+
 };
 
 #endif // POPULATION_H
